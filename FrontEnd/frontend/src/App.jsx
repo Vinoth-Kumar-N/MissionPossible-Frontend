faba6a68685dd2f857af6a56338f2d406a277176
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-10-04 22:21:09 +0530

    vanakam

---
+++ FrontEnd/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div className='bg-orange-400'>
+      <p className='font-mono text-5xl'>heeloo</p>
+    </div>
+  )
+}
+
+export default App
