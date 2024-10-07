5d24d0dc914b1e57421cf36435c99b3d2ecbd21e
Jagadheesh-007 <jagadheesh2005@gmail.com>
2024-10-07 08:04:33 +0530

    photo changed

--- MImp-Client/src/components/Home.jsx
+++ MImp-Client/src/components/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
    return (
       <>
          <div className='w-screen h-screen z-0 mt-[-1%]'>
-            <div className='w-[100%] h-[92vh] bg-[url("../assets/img/imagebg.jpg")] bg-cover bg-no-repeat  flex flex-col items-center font-bold'>
-               <div className='text-3xl mt-[5%]'> Tamil Nadu's Best</div>
-               <div className='text-3xl'>Travel Itinerary Planner</div>
+            <div className='w-[100%] h-[92vh] bg-[url("../assets/img/home.png")] bg-cover bg-no-repeat  flex flex-col items-center font-bold'>
+               <div className='text-3xl mt-[5%] text-blue-700'> Tamil Nadu's Best</div>
+               <div className='text-3xl text-blue-700'>Travel Itinerary Planner</div>
                <div className='flex space-x-1 mt-[5%]'>
                   <div className='flex space-x-1'><Star color='orange' fill='orange' /><Star color='orange' fill='orange' /><Star color='orange' fill='orange' /><Star color='orange' fill='orange' /><LucideStarHalf color='orange' fill='orange' /></div>
                   <div className='font-bold'>4.5 + Ratings</div>
