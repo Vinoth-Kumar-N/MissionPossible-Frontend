26dfe577b9d793232a543deea8d654a39520a937
Ponnarasu <ponnarasua22it@srishakthi.ac.in>
2024-10-07 11:33:44 +0530

    updated home

--- MImp-Client/src/components/Home.jsx
+++ MImp-Client/src/components/Home.jsx
@@ -15,10 +15,18 @@ const Home = () => {
                   <div className='flex space-x-1'><Star color='orange' fill='orange' /><Star color='orange' fill='orange' /><Star color='orange' fill='orange' /><Star color='orange' fill='orange' /><LucideStarHalf color='orange' fill='orange' /></div>
                   <div className='font-bold text-blue-700'>4.5 + Ratings</div>
                </div>
+                  <div className=" mt-8 flex flex-col justify-center items-center gap-6">
+                     <p className="text-[3rem] text-red-500 font-bold">Discover your Next Adventure with Us</p>
+                     <p className="text-[3rem] text-black font-bold">Itineraries at your Fingertips</p>
+                     <p className="text-[16px] text-[#3d4dad]">Your personal trip planner and travel curator, creating otineraries tailored to your interest</p>
+                  </div>
+                  <div className="mt-4">
+                     <button className="w-[7rem] h-[3rem] bg-black text-white rounded-md"><a href="/createtrip" className="">Get Started</a></button>
+                  </div>
             </div>
-            <InfoPage name={'erode'}/>
-            <Cards/>
-            <Map/>
+            <InfoPage name={'erode'} />
+            <Cards />
+            <Map />
             <div className='w-screen h-screen flex flex-wrap justify-center'>
                <div className=' bg-slate-400 flex w-[150px] justify-center rounded-lg h-8'>
                   <button className='text-black'>View More</button><ArrowRight className='text-black' />
