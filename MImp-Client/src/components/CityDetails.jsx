43cef5584cd498d948fd3d769a135460c464a177
Jagadheesh-007 <jagadheesh2005@gmail.com>
2024-10-08 10:16:29 +0530

    City page responsiveness added

--- MImp-Client/src/components/CityDetails.jsx
+++ MImp-Client/src/components/CityDetails.jsx
@@ -1,24 +1,30 @@
 import React from "react";
+
 const CityDetails = () => {
   return (
     <>
-      <div className="flex h-full w-full justify-center">
-        <div className="h-[600px] w-[90%] mt-8 flex bg-slate-500 rounded-lg">
-          <div className="w-1/3 flex justify-center items-center">
+      <div className="flex h-full w-full justify-center px-4 sm:px-8 lg:px-16">
+        <div className="h-auto w-full sm:w-[90%] mt-8 flex flex-col lg:flex-row bg-slate-500 rounded-lg">
+          {/* Image Section */}
+          <div className="w-full lg:w-1/3 flex justify-center items-center">
             <img
               src=""
               alt="City Image"
-              className="h-[500px] w-[350px] bg-slate-300 rounded-xl"
+              className="h-[300px] w-[90%] sm:h-[370px] sm:w-[350px] sm:mt-5 bg-slate-300 rounded-xl"
             />
           </div>
-          <div className="w-2/3 flex justify-center items-center">
-            <div className="w-[80%] h-[80%] bg-slate-300 rounded-xl flex justify-center flex-col items-center gap-3">
-              <div className="w-[80%] h-[20%] bg-slate-50 rounded-md">Name</div>
-              <div className="h-[40%] w-[80%] bg-slate-50 rounded-md">
+
+          {/* Details Section */}
+          <div className="w-full lg:w-2/3 flex justify-center items-center mt-6 lg:mt-0">
+            <div className="w-full sm:w-[80%] h-auto sm:h-[80%] bg-slate-300 rounded-xl flex justify-center flex-col items-center gap-3 p-4">
+              <div className="w-full h-[50px] sm:h-[20%] bg-slate-50 rounded-md flex items-center justify-center">
+                Name
+              </div>
+              <div className="h-[100px] sm:h-[40%] w-full bg-slate-50 rounded-md flex items-center justify-center">
                 Description
               </div>
-              <div className="h-[20%] w-[80%] bg-slate-50 rounded-md">
-                Therila
+              <div className="h-[50px] sm:h-[20%] w-full bg-slate-50 rounded-md flex items-center justify-center">
+                Price
               </div>
             </div>
           </div>
