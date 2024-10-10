407f4ba462b20268c71bb23a4f34b0eed9b95db8
Vinoth-Kumar-N <vinothkumarn22it@srishakthi.ac.in>
2024-10-10 19:10:39 +0530

    api configured

--- MImp-Client/src/services/api.js
+++ MImp-Client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = 'http://localhost:1111';
+const API = 'https://missionpossible-backend-ipd0.onrender.com';
 
 const RegisUser = (data) => axios.post(`${API}/user/register`, data);
 const LoginUser = (data) => axios.post(`${API}/user/login`, data);
