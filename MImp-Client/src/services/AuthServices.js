946d9977a451751b913d1b2001dc67d0b32041ae
gitvinoth <natarajvinoth7@gmail.com>
2024-12-09 18:33:38 +0530

    Update AuthServices.js

--- MImp-Client/src/services/AuthServices.js
+++ MImp-Client/src/services/AuthServices.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { getUserdata, removeUserdata } from '../services/storageServices';
 const API = 'https://missionpossible-backend-6x8r.onrender.com';
+             
 // const API = 'http://localhost:1111';
 
 const RegisUser = (data) => axios.post(`${API}/user/register`, data);
